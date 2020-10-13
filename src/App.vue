ae9652b6e50babc077cf9fca867dc2f80690acec
Alejandro Perez <alex@Alejandros-MacBook-Pro.local>
2020-10-13 15:37:10 -0400

    First Commit - Vue CLI && Company Logo

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <RpiContainer />
+  </div>
+</template>
+
+<script>
+import RpiContainer from './components/RPIContain.vue'
+
+export default {
+  name: 'App',
+  components: {
+    RpiContainer
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
