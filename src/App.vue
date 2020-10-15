522ec7eaa7ba95d72f86e6b78b3180214691e511
Alejandro Perez <alex@Alejandros-MacBook-Pro.local>
2020-10-15 16:13:07 -0400

    CSS Custom Properties, Nav added

--- src/App.vue
+++ src/App.vue
@@ -16,12 +16,21 @@ export default {
 </script>
 
 <style>
+:root {
+  --d-blue: #076099;
+  --l-blue: #057dca;
+  --white: #f1f1f1;
+  --d-gray: #2b2b2b;
+}
+
+* {
+  box-sizing: border-box;
+  margin: 0;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/RPINav.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="nav-wrap">
+        <img src="../assets/logo.png" alt="Real Property Investing Logo">
+        <ul>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Nav',
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
